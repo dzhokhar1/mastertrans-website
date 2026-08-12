4fa08a83e751c8b976953fbadcc6150ec9cd3562
ataevdj <ataevdj+arunblec@arunblec.live>
2026-08-12 18:48:28 +0300

    Fix responsive image paths

--- rebrand_home.py
+++ rebrand_home.py
@@ -13,6 +13,7 @@ html = re.sub(r'<script\b[^>]*>.*?</script>', '', html, flags=re.S | re.I)
 # CSS asset URLs would otherwise resolve against dzhokhar1.github.io itself.
 # Keep assets relative so the same output works both locally and on Pages.
 html = re.sub(r'url\((?P<quote>["\']?)/', r'url(\g<quote>', html)
+html = re.sub(r'(?P<attribute>srcset=["\'])/', r'\g<attribute>', html)
 
 calculator_head = '''<div class="vz-calculator-main-grid"><div class="fs-16 text-medium vz-calculator-main-title">Рассчитать цену / Заказать перевозку</div><div></div><div class="vz-switcher vz-switcher-rounded"><div class="vz-switcher-wrapper"><div class="vz-tooltip vz-tooltip-flex"><div class="vz-switcher-item active"><span>Рассчитать</span></div></div><div class="vz-tooltip vz-tooltip-flex"><div class="vz-switcher-item"><span>Отследить</span></div></div></div></div></div>'''
 html = re.sub(r'<div class="vz-calculator-main-grid"><div class="fs-16 text-medium vz-calculator-main-title">Рассчитать цену / Заказать перевозку</div><div></div><span></span></div>', calculator_head, html, count=1)
